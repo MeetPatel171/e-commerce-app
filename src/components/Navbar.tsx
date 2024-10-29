84b011e2d0fe00e0bbab393a5f12161b9109a3e2
MeetPatel171 <meetpatel1117@icloud.com>
2024-10-29 15:47:18 -0400

    Refactor layout and add Navbar and Footer components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,40 +6,40 @@ import NavIcons from "./NavIcons";
 
 const Navbar = () => {
     return (
-      <div className= "h-20 px-4 md:px-8 l:px-16 xl:px-32 2xl:px-64">
+        <div className="h-20 px-4 md:px-8 l:px-16 xl:px-32 2xl:px-64">
 
-        {/*MOBILE*/}
-        <div className="h-full flex items-center justify-between md:hidden">
-        <div className="text-2xl tracking-wide">LAMA</div>
-          <link href="/" rel="stylesheet"></link>
-          <Menu/>
+            {/*MOBILE*/}
+            <div className="h-full flex items-center justify-between md:hidden">
+                <div className="text-2xl tracking-wide">Cultured Paradise</div>
+                <link href="/" rel="stylesheet"></link>
+                <Menu />
+            </div>
+
+            {/*BIGGER SCREENS*/}
+            <div className="hidden md:flex items-center justify-between gap-8 h-full">
+
+                {/*LEFT*/}
+                <div className="w-1/3">
+                    <Link href="/" className="flex item-center gap-3">
+                        <Image src="/logo.png" alt="logo" width={24} height={24} />
+                        <div className="text-2xl tracking-wide">Cultured Paradise</div>
+                    </Link>
+                </div>
+
+
+                {/*RIGHT*/}
+                <div className="w-2/3 flex items-center justify-between gap-8">
+                    <SearchBar />
+                    <NavIcons />
+                </div>
+
+
+
+            </div>
+
         </div>
 
-        {/*BIGGER SCREENS*/}
-        <div className="hidden md:flex items-center justify-between gap-8 h-full">
-
-          {/*LEFT*/}
-          <div className="w-1/3">
-            <Link href="/" className="flex item-center gap-3">
-              <Image src="/logo.png" alt="logo" width={24} height={24} />
-              <div className="text-2xl tracking-wide">LAMA</div>
-            </Link>
-          </div>
-
-
-          {/*RIGHT*/}
-          <div className="w-2/3 flex items-center justify-between gap-8">
-            <SearchBar/>
-            <NavIcons/>
-          </div>
-
-          
-          
-        </div>
-
-    </div>
-      
     )
-  }
-  
-  export default Navbar;
+}
+
+export default Navbar;
