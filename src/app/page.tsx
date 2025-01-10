0d8bb6ade6e45973fad84725f506160a9d4aa673
MeetPatel171 <meetpatel1117@icloud.com>
2025-01-10 15:09:41 -0500

    Added Categories, New products and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,17 @@ const HomePage = () => {
   return (
     <div className=''>
       <Slider />
-      <div className="m-top px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
         <ProductList />
       </div>
-      <div className="m-top px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1 className="text-2xl">Categories</h1>
+      <div className="mt-24">
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">Categories</h1>
         <CategoryList />
       </div>
-      <div className="m-top px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
-
+        <ProductList />
       </div>
 
     </div>
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 const slides = [
     {
         id: 1,
-        title: "Summer Sale Collections",
+        title: "Sale Collections",
         description: "Sale! Up to 50% off!",
         img: "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800",
         url: "/",
@@ -15,15 +15,15 @@ const slides = [
     },
     {
         id: 2,
-        title: "Winter Sale Collections",
+        title: "Sale Collections",
         description: "Sale! Up to 50% off!",
-        img: "https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800",
+        img: "https://images.pexels.com/photos/30095407/pexels-photo-30095407/free-photo-of-casual-clothing-rack-with-cap-and-hoodie-display.jpeg?auto=compress&cs=tinysrgb&w=800",
         url: "/",
         bg: "bg-gradient-to-r from-pink-50 to-blue-50",
     },
     {
         id: 3,
-        title: "Spring Sale Collections",
+        title: "Sale Collections",
         description: "Sale! Up to 50% off!",
         img: "https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800",
         url: "/",
